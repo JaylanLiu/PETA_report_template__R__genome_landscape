2002bc4bfd0e84771b49647ca9a82d0e35cc404a
Jilong Liu <liujilong@genomics.cn>
2020-08-03 17:52:12 +0800

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PETA_report_template__R__genome_landscape
 PETA report template using IR kernel to illustrate genome landscape
+
+## Usage
+1. PETA_report_template__R__genome_landscape.ipynb is the template demo, details that should be paid attention to are written in the markdown cells.
+2. Run the template in command line using run.sh.
+3. The output report is attached as PETA_report_template__R__genome_landscape.html.
+
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#Email:liujilong@genomics.cn
+#Description:
+#Edit History:
+#2020-08-03 17:18:14  File created.
+jwr -i PETA_report_template__R__genome_landscape.ipynb -o PETA_report_template__R__genome_landscape.html --json_str '{"studyIds":["blca_mskcc_solit_2014"],"pageSize":999999,"pageIndex":1,"attributesRangeFilters":[],"attributesEqualFilters":[],"mutationFilter":{"hugoGeneSymbols":[],"exacStart":0,"exadEnd":1,"vabundStart":0,"vabundEnd":1,"variantSource":[],"variantType":[],"variantClass":[],"sequencer":[],"sequencerSource":[],"searchStr":""}}' --token '02c67819-12f8-49a0-928f-92dde0e609ae'
